2f2c6906f7bc3b12d340f1b5e367906bf5c542ba
Dave Smith <getdavemail@gmail.com>
2019-11-13 10:40:58 +0000

    Adds classname to signify consumption of toolbars

--- packages/block-editor/src/components/inner-blocks/index.js
+++ packages/block-editor/src/components/inner-blocks/index.js
@@ -115,6 +115,7 @@ class InnerBlocks extends Component {
 
 		const classes = classnames( 'editor-inner-blocks block-editor-inner-blocks', {
 			'has-overlay': enableClickThrough && hasOverlay,
+			'is-consuming-toolbars': consumeChildToolbar,
 		} );
 
 		return (
