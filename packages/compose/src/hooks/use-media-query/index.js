115251de7f6e7f5ef1773965834dc090b42d6d30
Jorge <jorge.costa@developer.pt>
2019-12-09 12:11:57 +0000

    Add mechanism to set a width on useViewportMatch

--- packages/compose/src/hooks/use-media-query/index.js
+++ packages/compose/src/hooks/use-media-query/index.js
@@ -6,20 +6,28 @@ import { useState, useEffect } from '@wordpress/element';
 /**
  * Runs a media query and returns its value when it changes.
  *
- * @param {string} query Media Query.
+ * @param {?string} query Media Query.
  * @return {boolean} return value of the media query.
  */
 export default function useMediaQuery( query ) {
 	const [ match, setMatch ] = useState( false );
 	useEffect( () => {
+		if ( ! query ) {
+			if ( match ) {
+				setMatch( false );
+			}
+			return;
+		}
 		const updateMatch = () => setMatch( window.matchMedia( query ).matches );
 		updateMatch();
 		const list = window.matchMedia( query );
 		list.addListener( updateMatch );
 		return () => {
-			list.removeListener( updateMatch );
+			if ( list ) {
+				list.removeListener( updateMatch );
+			}
 		};
-	}, [ query ] );
+	}, [ match, query ] );
 
 	return match;
 }
